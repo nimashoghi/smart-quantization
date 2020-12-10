b7d04a0a4c57ef1c86c4700c369e5b34375b2afe
Nima Shoghi <nimashoghi@gmail.com>
2020-12-10 14:18:30 +0000

    Removed unsupported backward hook

--- smart_compress/util/pytorch/hooks.py
+++ smart_compress/util/pytorch/hooks.py
@@ -6,10 +6,7 @@ import torch
 import torch.nn as nn
 from smart_compress.util.pytorch.optimizer import OptimLP
 from smart_compress.util.pytorch.quantization import check_layer_types
-from torch.nn.modules.module import (
-    register_module_backward_hook,
-    register_module_forward_hook,
-)
+from torch.nn.modules.module import register_module_forward_hook
 from torch.utils.hooks import RemovableHandle
 
 DEFAULT_LAYER_TYPES = ["conv", "linear", "pool", "normalization"]
@@ -42,29 +39,12 @@ def _register_forward_hook(compress_fn, hparams, layer_types=DEFAULT_LAYER_TYPES
     return register_module_forward_hook(forward_hook)
 
 
-def _register_backward_hook(compress_fn, hparams, layer_types=DEFAULT_LAYER_TYPES):
-    def backward_hook(module: nn.Module, _: Tuple[torch.Tensor], output: torch.Tensor):
-        if type(output) != torch.Tensor or not check_layer_types(
-            module, layer_types=layer_types
-        ):
-            return None
-
-        return compress_fn(output, hparams)
-
-    return register_module_backward_hook(backward_hook)
-
-
 def register_global_hooks(
-    compress_fn, hparams, layer_types=DEFAULT_LAYER_TYPES, forward=True, backward=True
+    compress_fn, hparams, layer_types=DEFAULT_LAYER_TYPES, forward=True
 ):
     hooks: List[RemovableHandle] = []
     if forward:
         hooks.append(
             _register_forward_hook(compress_fn, hparams, layer_types=layer_types)
         )
-    if backward:
-        hooks.append(
-            _register_backward_hook(compress_fn, hparams, layer_types=layer_types)
-        )
-
     return hooks
