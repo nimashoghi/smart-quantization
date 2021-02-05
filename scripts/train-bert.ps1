d3fcd1d18b6e36a34ab6e8273187049f293fe4c5
Nima Shoghi <nimashoghi@gmail.com>
2021-02-05 21:12:45 +0000

    Updated

--- scripts/train-bert.ps1
+++ scripts/train-bert.ps1
@@ -1,16 +1,16 @@
 # forward + backward
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 6 --num_bits_outlier 8 --measure_compression_ratio @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress fp8 @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress s2fp8 --measure_compression_ratio @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress fp16 @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress bf16 @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress fp32 @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 5 --num_bits_outlier 7 --measure_compression_ratio @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 4 --num_bits_outlier 6 --measure_compression_ratio @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 3 --num_bits_outlier 5 --measure_compression_ratio @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 6 --num_bits_outlier 8 --measure_compression_ratio @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress fp8 @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress s2fp8 --measure_compression_ratio @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress fp16 @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress bf16 @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress fp32 @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 5 --num_bits_outlier 7 --measure_compression_ratio @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 4 --num_bits_outlier 6 --measure_compression_ratio @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 3 --num_bits_outlier 5 --measure_compression_ratio @args
 
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 6 --num_bits_outlier 8 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress s2fp8 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 5 --num_bits_outlier 7 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 4 --num_bits_outlier 6 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
-python ./train.py --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 3 --num_bits_outlier 5 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 6 --num_bits_outlier 8 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress s2fp8 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 5 --num_bits_outlier 7 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 4 --num_bits_outlier 6 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
+python ./train.py --git --accelerator ddp --gpus -1 --model bert --dataset glue --batch_size 10 --val_batch_size 2 --max_epochs 100 --compress smart --num_bits_main 3 --num_bits_outlier 5 --measure_compression_ratio --no_compress_momentum_vectors --no_compress_backward @args
