974579dd3d3719e0396f10accda808d7877a11de
Nima Shoghi <nimashoghi@gmail.com>
2021-04-28 18:15:03 +0000

    C++11

--- hw/smaq.cpp
+++ hw/smaq.cpp
@@ -2,14 +2,23 @@
 #include <cmath>
 #include <utility>
 
+#include <ap_int.h>
+
 constexpr auto N = 1024;
+
+struct axi_interface_type
+{
+    ap_uint<32> data;
+    ap_int<1> last;
+};
+
 using array_t = std::array<float, N>;
 using quantized_array_t = std::array<int, N>;
 
 // constexpr auto N = 256;
 // using array_t = std::vector<float>;
 
-static inline auto get_stats(const array_t &array)
+static inline std::pair<float, float> get_stats(const array_t &array)
 {
     float sum = 0.f;
     float sum_of_squares = 0.f;
@@ -26,14 +35,14 @@ static inline auto get_stats(const array_t &array)
     return std::make_pair(m1, std::sqrt(m2 - (m1 * m1)));
 }
 
-constexpr auto get_scale(const std::size_t n_bits) {
+constexpr float get_scale(const std::size_t n_bits) {
     return float(1 << (n_bits - 1)) / 3.f;
 }
 
-constexpr auto scale_1std = get_scale(6);
-constexpr auto scale_else = get_scale(8);
+constexpr float scale_1std = get_scale(6);
+constexpr float scale_else = get_scale(8);
 
-constexpr auto quantize(const auto data) {
+inline int quantize(const float data) {
     const auto is_1std = (data <= 1.f && data >= -1.f);
     const auto scale =
         is_1std ? scale_1std : scale_else;
@@ -41,17 +50,17 @@ constexpr auto quantize(const auto data) {
     return int(quantized_float) << 1 | (is_1std ? 0b1 : 0b0);
 }
 
-constexpr auto dequant_scale_1std = 1.f / get_scale(6);
-constexpr auto dequant_scale_else = 1.f/ get_scale(8);
+constexpr float dequant_scale_1std = 1.f / get_scale(6);
+constexpr float dequant_scale_else = 1.f/ get_scale(8);
 
-constexpr auto dequantize(const int data) {
-    const auto is_1std = (data & 0b1);
-    const auto scale =
+inline float dequantize(const int data) {
+	const auto is_1std = (data & 0b1);
+	const auto scale =
         is_1std ? dequant_scale_1std : dequant_scale_else;
     return float(data >> 1) * scale;
 }
 
-quantized_array_t compress(array_t array)
+inline quantized_array_t compress(array_t array)
 {
     quantized_array_t return_array = {0};
 
@@ -59,20 +68,65 @@ quantized_array_t compress(array_t array)
 
     const auto sum = 0;
     for (auto i = 0u; i < array.size(); ++i) {
-        return_array[i] = quantize((array[i] - stats.first) / stats.second);
+        const auto z_score = (array[i] - stats.first) / stats.second;
+        return_array[i] = quantize(z_score);
     }
 
     return return_array;
 }
 
-array_t decompress(quantized_array_t array, const float mean, const float std_dev)
+inline array_t decompress(quantized_array_t array, const float mean, const float std_dev)
 {
     array_t return_array = {0};
 
     const auto sum = 0;
     for (auto i = 0u; i < array.size(); ++i) {
-        return_array[i] = (dequantize(array[i]) * std_dev) + mean;
+        const auto z_score = dequantize(array[i]);
+        return_array[i] = (z_score * std_dev) + mean;
     }
 
     return return_array;
+}
+
+extern "C"
+{
+    void compress_accel(axi_interface_type *src, axi_interface_type *dst)
+    {
+#pragma HLS INTERFACE axis register both port=src
+#pragma HLS INTERFACE axis register both port=dst
+#pragma HLS INTERFACE s_axilite port=return
+
+        array_t array = {0};
+
+        for (auto i = 0u; i < array.size(); ++i) {
+            array[i] = src[i].data;
+        }
+
+        const auto return_array = compress(array);
+        for (auto i = 0u; i < return_array.size(); ++i) {
+            dst[i].data = return_array[i];
+            dst[i].last = i == return_array.size();
+        }
+    }
+
+    void decompress_accel(const float mean, const float std_dev, axi_interface_type *src, axi_interface_type *dst)
+    {
+#pragma HLS INTERFACE s_axilite port=mean
+#pragma HLS INTERFACE s_axilite port=std_dev
+#pragma HLS INTERFACE axis register both port=src
+#pragma HLS INTERFACE axis register both port=dst
+#pragma HLS INTERFACE s_axilite port=return
+
+        quantized_array_t array = {0};
+
+        for (auto i = 0u; i < array.size(); ++i) {
+            array[i] = src[i].data;
+        }
+
+        const auto return_array = decompress(array, mean, std_dev);
+        for (auto i = 0u; i < return_array.size(); ++i) {
+            dst[i].data = return_array[i];
+            dst[i].last = i == return_array.size();
+        }
+    }
 }
